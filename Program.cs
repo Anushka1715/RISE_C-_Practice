12615bef475e8ce8cce79fc2be067c5be3c2247c
Anushka1715 <anushkamahajan1715@gmail.com>
2024-03-16 17:23:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RISE_C__Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
