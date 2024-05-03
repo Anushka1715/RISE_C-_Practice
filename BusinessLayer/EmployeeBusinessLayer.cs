1fe9099fbbadd2f7730561b59ebc53d0b8de912c
Anushka1715 <anushkamahajan1715@gmail.com>
2024-05-03 11:08:16 +0530

    commit businessModel

---
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessLayer
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetAllEmployees()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+
+            List<Employee> employees = new List<Employee>();
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {   
+                SqlCommand cmd = new SqlCommand("GetEmployees", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.Empid = Convert.ToInt32(rdr["Empid"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.City = rdr["City"].ToString();
+                    employee.Address = rdr["Address"].ToString();
+
+                    employees.Add(employee);
+                }
+                return employees;
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+
+            List<Employee> employees = new List<Employee>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("AddNewEmpDetails", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paraName = new SqlParameter();
+                paraName.ParameterName = "Name";
+                paraName.Value = employee.Name; 
+                cmd.Parameters.Add(paraName);
+
+                SqlParameter paraCity = new SqlParameter();
+                paraCity.ParameterName = "City";
+                paraCity.Value = employee.City;
+                cmd.Parameters.Add(paraCity);
+
+                SqlParameter paraAddress = new SqlParameter();
+                paraAddress.ParameterName = "Address";
+                paraAddress.Value = employee.City;
+                cmd.Parameters.Add(paraAddress);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+
+            List<Employee> employees = new List<Employee>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("UpdateEmpDetails", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paraId = new SqlParameter();
+                paraId.ParameterName = "@EmpId";
+                paraId.Value = employee.Empid;
+                cmd.Parameters.Add(paraId);
+
+                SqlParameter paraName = new SqlParameter();
+                paraName.ParameterName = "@Name";
+                paraName.Value = employee.Name;
+                cmd.Parameters.Add(paraName);
+
+                SqlParameter paraCity = new SqlParameter();
+                paraCity.ParameterName = "@City";
+                paraCity.Value = employee.City;
+                cmd.Parameters.Add(paraCity);
+
+                SqlParameter paraAddress = new SqlParameter();
+                paraAddress.ParameterName = "@Address";
+                paraAddress.Value = employee.Address;
+                cmd.Parameters.Add(paraAddress);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+
+            List<Employee> employees = new List<Employee>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("DeleteEmpById", connection);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paraId = new SqlParameter();
+                paraId.ParameterName = "@EmpId";
+                paraId.Value = id;
+                cmd.Parameters.Add(paraId);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
---
+++ MVC_BusinessLogic/Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace MVC_BusinessLogic.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new BusinessLayer.EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusinessLayer.GetAllEmployees();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            Employee employee = new Employee();
+            employee.Name = formCollection["Name"];
+            employee.City = formCollection["City"];
+            employee.Address = formCollection["Address"];
+
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();  
+            employeeBusinessLayer.AddEmployee(employee);
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int Empid) {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            Employee employee = employeeBusinessLayer.GetAllEmployees().FirstOrDefault(emp => emp.Empid == Empid);
+            return View(employee);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+                employeeBusinessLayer.UpdateEmployee(employee);
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            employeeBusinessLayer.DeleteEmployee(id);
+
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
